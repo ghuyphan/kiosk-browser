ee5402a832249f214bc73429cad8574bb33fdcef
shorkyyy <giahuyphan0110@gmail.com>
2026-06-12 09:22:05 +0700

    Refresh launcher icon and splash branding

--- app/src/main/java/com/qmsbrowser/MainActivity.java
+++ app/src/main/java/com/qmsbrowser/MainActivity.java
@@ -104,6 +104,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         buildBrowser();
         applyPreferences(false);
